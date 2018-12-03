049ec9717b082acb5b56aa889037798f3793f1d8
Kevin Geng <khg@andrew.cmu.edu>
2018-12-03 00:09:22 -0500

    Add files for day 3

---
+++ 3/solve.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import Counter, defaultdict, deque
+from itertools import permutations, combinations, product
+
+def parse_claim(line):
+    line = line.split()
+    d = int(line[0].lstrip('#'))
+    left, top = map(int, line[2].rstrip(':').split(','))
+    width, height = map(int, line[3].split('x'))
+    return d, left, top, width, height
+
+def main(A):
+    A = [parse_claim(line) for line in A]
+    grid = defaultdict(int)
+    for claim in A:
+        d, l, t, w, h = claim
+        for i in range(h):
+            for j in range(w):
+                coords = (t+i, l+j)
+                grid[coords] += 1
+
+    count = 0
+    for k, v in grid.items():
+        if v >= 2:
+            count += 1
+    print('Part 1', count)
+
+    for claim in A:
+        d, l, t, w, h = claim
+        has_overlap = False
+        for i in range(h):
+            for j in range(w):
+                coords = (t+i, l+j)
+                if grid[coords] > 1:
+                    has_overlap = True
+        if not has_overlap:
+            print('Part 2', d)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2 and sys.argv[1].startswith('s'):
+        A = open('sample.txt').read()
+        A = A.splitlines()
+    else:
+        A = open('input.txt').read()
+        A = A.splitlines()
+    main(A)
