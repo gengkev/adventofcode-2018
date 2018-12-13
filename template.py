33c63bf2481114c9a102ad41e392d5177aa85660
Kevin Geng <khg@andrew.cmu.edu>
2018-12-13 00:43:33 -0500

    Update template

--- template.py
+++ template.py
@@ -5,23 +5,32 @@ import sys
 from collections import Counter, defaultdict, deque
 from itertools import permutations, combinations, product
 
+is_sample = False
+
+
 def parse_ints(text):
     "All the integers anywhere in text."
-    return [int(x) for x in re.findall(r'\d+', text)]
+    return [int(x) for x in re.findall(r'-?\d+', text)]
+
+
+def parse_line(line):
+    #return line.split()
+    #return parse_ints(line)
+    return line
 
 
 def main(A):
-    #A = [line.split() for line in A]
-    #A = [int(line) for line in A]
-    #A = [parse_ints(line) for line in A]
-    print('Part 1', sum(A))
+    A = A.splitlines()
+    A = [parse_line(line) for line in A]
+    
+    res = 0
+    print('Part 1', res)
 
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2 and sys.argv[1].startswith('s'):
         A = open('sample.txt').read()
-        A = A.split(', ')
+        is_sample = True
     else:
         A = open('input.txt').read()
-        A = A.splitlines()
     main(A)
